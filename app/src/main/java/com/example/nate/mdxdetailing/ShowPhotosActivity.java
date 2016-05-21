908ceb05b4a558f6f5df9edb49dae0ea79d2d9c5
Nate <nbovino05@gmail.com>
2016-05-21 18:13:52 -0400

    Version 1.0 - Complete app. Can show service descriptions and photos. Can add/change descriptions and photos as needed. Later let user add/change these

--- app/src/main/java/com/example/nate/mdxdetailing/ShowPhotosActivity.java
+++ app/src/main/java/com/example/nate/mdxdetailing/ShowPhotosActivity.java
@@ -1,54 +1,62 @@
 package com.example.nate.mdxdetailing;
 
-import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.nate.mdxdetailing.R;
-import com.example.nate.mdxdetailing.adapters.ServiceAdapter;
-import com.example.nate.mdxdetailing.InteriorService;
+import com.example.nate.mdxdetailing.adapters.PhotoAdapter;
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ShowServicesActivity extends AppCompatActivity {
+public class ShowPhotosActivity extends AppCompatActivity {
 
-//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                //String dayOfTheWeek = mDays[position].getDayOfTheWeek();
-//                //String condition = mDays[position].getSummary();
-//                //String highTemp = mDays[position].getTemperatureMax() + "";
-//                //String message = String.format("On %s the high will be %s and it will be %s",
-//                //        dayOfTheWeek, highTemp, condition);
-//                //Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG).show();
-//            }
-//        });
+//    private PhotoCollection[] mPhoto;
+//
+//    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_show_photos);
+//        ButterKnife.bind(this);
+//
+//        Intent intent = getIntent();
+//        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.PHOTOS);
+//        mPhoto = Arrays.copyOf(parcelables, parcelables.length, PhotoCollection[].class);
+//
+//        PhotoAdapter adapter = new PhotoAdapter(this, mPhoto);
+//        mRecyclerView.setAdapter(adapter);
+//
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(layoutManager);
+//        mRecyclerView.setHasFixedSize(true);
+//    }
 
-    private Service[] mServices;
 
-    @Bind(android.R.id.list) ListView mListView;
+    private PhotoCollection[] mPhotos;
+
+    @Bind(android.R.id.list)
+    ListView mListView;
     // This must be Nullable or else it throws an exception. I don't know why
-    @Nullable @Bind(android.R.id.empty) TextView mEmptyTextView;
+    @Nullable
+    @Bind(android.R.id.empty)
+    TextView mEmptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_services);
+        setContentView(R.layout.activity_show_photos);
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
@@ -57,11 +65,12 @@ public class ShowServicesActivity extends AppCompatActivity {
         String pageHeading = intent.getStringExtra("service");
         serviceType.setText(pageHeading);
 
-        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.SERVICE);
-        mServices = Arrays.copyOf(parcelables, parcelables.length, Service[].class);
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.PHOTOS);
+        mPhotos = Arrays.copyOf(parcelables, parcelables.length, PhotoCollection[].class);
 
-        ServiceAdapter adapter = new ServiceAdapter(this, mServices);
+        PhotoAdapter adapter = new PhotoAdapter(this, mPhotos);
         mListView.setAdapter(adapter);
+        mListView.setDivider(null);
         mListView.setEmptyView(mEmptyTextView);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
